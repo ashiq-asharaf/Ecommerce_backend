5bf58ac689745877c925c53c9b023203bb066613
Ashiq A <ashiq.ashraf@ilearningengines.com>
2024-03-28 15:19:37 +0530

    LoginEndpoint has added

--- Queries/queries.js
+++ Queries/queries.js
@@ -4,5 +4,17 @@ function userLogin(req, res, next) {
     const args=[];
     const argVals = [];
     let argCount = 1;
-    
+    argCount.push(`c_user_email => $${argCount} :: text`);
+    argVals.push(req.body.userEmail);
+    argCount += 1;
+    args.push(`c_user_pwd => $${argCount} :: text`);
+    argVals.push(req.body.memberPwd);
+    argCount += 1;
+    db.any(`SELECT * FROM ec_fn_user_login(${args.join(",")})`, argVals)
+    .then((data) => res.status(200).json(data))
+    .catch((err) => next(err));
+}
+
+module.exports = {
+    userLogin
 }
--- Routes/schema.js
+++ Routes/schema.js
@@ -1,2 +1,10 @@
 const joi = require('joi');
 
+const checkLogin = joi.object().keys({
+    memberLogin: joi.string().allow('').required(),
+    memberPwd: joi.string().allow('').required()
+  });
+
+  module.exports = {
+    checkLogin
+  }
